80298de303e296cd5cc5d889dc8b46de414f0d20
Marcus Anderson <marqui0688@gmail.com>
2023-04-03 20:50:45 -0400

    Establish footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.scss";
+
+export const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <div className="footer-content__container">
+          <p className="test">It's ya boy</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+export default Footer;
