25d30a95a941ee420898327eb5fd19edc8608fa9
Marcus Anderson <marqui0688@gmail.com>
2023-04-04 13:10:00 -0400

    Complete podcast list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import Footer from "../src/components/Footer";
+import PodcastList from "./components/PodcastList";
 
 function App() {
-  return <Footer />;
+  return (
+    <>
+      <Footer />
+      <PodcastList />
+    </>
+  );
 }
 
 export default App;
